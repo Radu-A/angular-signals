ff2819c5a20d6de7585a36758bd5289a1502ed88
Radu-A <elcastravete@gmail.com>
2026-01-13 19:51:58 +0100

    feat(app): create computed signals from userStatus

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,10 @@
 // TODO: Import signal from @angular/core
-import { Component, signal, ChangeDetectionStrategy } from "@angular/core";
+import {
+  Component,
+  signal,
+  computed,
+  ChangeDetectionStrategy,
+} from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -8,10 +13,32 @@ import { Component, signal, ChangeDetectionStrategy } from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  userStatus = signal<"online" | "offline">("offline");
+  userStatus = signal<"online" | "away" | "offline">("offline");
+  notificationsEnabled = computed(() => this.userStatus() === "online");
+  statusMessage = computed(() => {
+    const status = this.userStatus();
+    switch (status) {
+      case "online":
+        return "Available for meetings and messages";
+      case "away":
+        return "Temporarily away, willl respond soon";
+      case "offline":
+        return "Status unknown";
+    }
+  });
+  isWithinWorkingHours = computed(() => {
+    const now = new Date();
+    const isWorkingHour = now.getHours() >= 9 && now.getHours() < 17;
+    const isWeekday = now.getDay() > 0 && now.getDay() < 6;
+    return isWorkingHour && isWeekday && this.userStatus() != "offline";
+  });
 
   goOnline() {
     this.userStatus.set("online");
+  }
+
+  setAway() {
+    this.userStatus.set("away");
   }
 
   goOffline() {
@@ -19,8 +46,17 @@ export class App {
   }
 
   togglStatus() {
-    this.userStatus.update((current) =>
-      current === "online" ? "offline" : "online"
-    );
+    const current = this.userStatus();
+    switch (current) {
+      case "online":
+        this.userStatus.set("away");
+        break;
+      case "away":
+        this.userStatus.set("offline");
+        break;
+      case "offline":
+        this.userStatus.set("online");
+        break;
+    }
   }
 }
