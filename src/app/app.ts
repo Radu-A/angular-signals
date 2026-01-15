9787e8e638e5e9cd2b8147bf92503040c6545645
Radu-A <elcastravete@gmail.com>
2026-01-15 09:23:22 +0100

    feat(cart-store): add service with signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,30 @@
-// TODO: Import model from @angular/core
-import { Component, model, ChangeDetectionStrategy } from "@angular/core";
-import { CustomCheckbox } from "./custom-checkbox/custom-checkbox";
+// TODO: Import inject from @angular/core
+import { Component, ChangeDetectionStrategy, inject } from "@angular/core";
+import { CartStore } from "./cart-store";
+import { CartDisplay } from "./cart-display";
 
 @Component({
   selector: "app-root",
-  imports: [CustomCheckbox],
-  templateUrl: "./app.html",
+  imports: [CartDisplay],
+  template: `
+    <div class="shopping-app">
+      <header>
+        <h1>Signals with Services Demo</h1>
+        <div class="cart-badge">
+          Cart: {{ cartStore.totalQuantity() }} items (\${{
+            cartStore.totalPrice()
+          }})
+        </div>
+      </header>
+
+      <main>
+        <cart-display />
+      </main>
+    </div>
+  `,
   styleUrl: "./app.css",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  agreedToTerms = model(false);
-  eneableNotifications = model(true);
-
-  toggleTermsFromParent() {
-    this.agreedToTerms.set(!this.agreedToTerms());
-  }
-
-  resetAll() {
-    this.agreedToTerms.set(false);
-    this.eneableNotifications.set(false);
-  }
+  cartStore = inject(CartStore);
 }
